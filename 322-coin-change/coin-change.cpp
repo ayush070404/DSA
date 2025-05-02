bcbf47702b3f693147e79e8766b10999b69a1191
ayush070404 <ayushkbgs4@gmail.com>
2025-05-02 12:10:39 +0530

    Time: 51 ms (25.17%) | Memory: 49.5 MB (23.63%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,38 +1,30 @@
 class Solution {
 public:
 
-    int solve(int ind, vector<int> &coins, int amt, vector<vector<int>> &dp) {
-        if (amt == 0) return 0;  
-    
-        if (ind == 0) {
-            if (amt % coins[0] == 0) return amt / coins[0];
-            return INT_MAX;  
+    int solve(int ind , vector<int> &coins , int amt , vector<vector<int >> &dp){
+        if(ind == 0) {
+            if( amt % coins[0] == 0) return  amt /coins[0];
+            return 1e9;
         }
 
-        if (dp[ind][amt] != -1) return dp[ind][amt];
+        if(dp[ind][amt] != -1) return dp[ind][amt];
 
-        int not_take = solve(ind - 1, coins, amt, dp);
-
+        int not_take = 0 + solve(ind -1 , coins , amt , dp);
         int take = INT_MAX;
-        if (coins[ind] <= amt) {
-            int subproblem = solve(ind, coins, amt - coins[ind], dp);
-            if (subproblem != INT_MAX) {  // Check if it's possible to use the coin
-                take = 1 + subproblem;
-            }
-        }
+        if(coins[ind] <= amt ) take = 1 + solve(ind , coins , amt - coins[ind], dp);
 
-        dp[ind][amt] = min(take, not_take);  
+        dp[ind][amt] = min(take , not_take);
 
         return dp[ind][amt];
     }
 
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector<vector<int>> dp(n, vector<int>(amount + 1, -1)); 
-        
-        int ans = solve(n - 1, coins, amount, dp);  
 
-        if (ans == INT_MAX) return -1;  
-        return ans;  // Return the result
+        vector<vector<int>> dp(n , vector<int> (amount+1 , -1));
+
+        int ans = solve( n-1 , coins , amount , dp);
+        if(ans >=1e9 ) return -1;
+        else return ans;
     }
 };
