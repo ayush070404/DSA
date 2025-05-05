9593e5a7788f2c8d6a659d28038b43bccdd6c725
ayush070404 <ayushkbgs4@gmail.com>
2025-05-05 10:54:34 +0530

    Time: 96 ms (46.76%) | Memory: 95.2 MB (34.01%) - LeetSync

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,31 +1,29 @@
 class Solution {
 public:
-
-    int solve(int ind, int buy, vector<int> &prices, int n,int fee,  vector<vector<int>> &dp){
-
-        if(ind>= n) return 0;
-        if(dp[ind][buy] !=-1) return dp[ind][buy];
-        int profit = 0;
-
-        if(buy){
-
-            profit= max(-prices[ind] - fee + solve(ind+1, 0, prices, n,fee,  dp),
-                        0 + solve(ind+1 , 1, prices, n,fee, dp)) ;
-        }
-
-        else{
-            profit= max(prices[ind]+ solve(ind+1, 1, prices, n,fee,  dp),
-                        0 + solve(ind+1 , 0, prices, n,fee, dp)) ;
-        }
-        
-        dp[ind][buy] = profit;
-        return dp[ind][buy];
-    }
-
-
     int maxProfit(vector<int>& prices, int fee) {
         int n = prices.size();
-        vector<vector<int>> dp(n +1 , vector<int> (2 , -1));
-        return solve(0 , 1, prices, n ,fee,  dp);
+        vector<vector<int>> dp(n +1 , vector<int> (2 , 0));
+        dp[n][0] = dp[n][1] = 0;
+        int profit = 0;
+
+
+        for(int ind = n-1 ; ind>= 0; ind--){
+            for(int buy = 0; buy <= 1; buy++){
+                if(buy){
+
+                    profit= max(-prices[ind]- fee + dp[ind+1][0],
+                                0 + dp[ind+1][1]);
+                }
+
+                else{
+                    profit= max(prices[ind] + dp[ind+1][1],
+                                0 + dp[ind+1][0]);
+                }
+            
+                dp[ind][buy] = profit;
+            }
+        }
+
+    return dp[0][1]; // why not dp[0][1] --> causing intially we are starting with nothing so, can buy
     }
 };
