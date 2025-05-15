8ea1635beb0b9a84fa1ba4b4bc210d7ea6cc3247
ayush070404 <ayushkbgs4@gmail.com>
2025-05-15 15:01:24 +0530

    Time: 15 ms (76.20%) | Memory: 26.2 MB (49.51%) - LeetSync

---
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+        int n = grid.size();
+        if(grid[0][0] != 0 || grid[n-1][n-1] != 0) return -1;
+
+        vector<vector<int>> vis(n, vector<int>(n, 0));
+        queue<pair<int, pair<int, int>>> q; // {steps, {row, col}}
+        q.push({1, {0, 0}});
+        vis[0][0] = 1;
+
+        int delrow[] = {-1,-1, 0,1,1,1, 0,-1};
+        int delcol[] = {0 ,1, 1,1,0 ,-1,-1,-1};
+
+        while(!q.empty()){
+            int r = q.front().second.first;
+            int c = q.front().second.second;
+            int steps = q.front().first;
+            q.pop();
+
+            if(r == n-1 & c == n-1) return steps;
+
+            for(int i = 0; i < 8 ; i++){
+                int row = r + delrow[i];
+                int col = c + delcol[i];
+
+                if(row >= 0 && row < n && col >= 0 && col < n && !vis[row][col] && grid[row][col] == 0){
+                    vis[row][col] = 1;
+                    q.push({steps+ 1 , {row ,col}});
+                }
+            } 
+        }
+    return -1;  
+    }
+};
