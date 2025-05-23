0f58e83aa6b3d9d62cd8c0df06c0291433e09315
ayush070404 <ayushkbgs4@gmail.com>
2025-05-23 11:44:56 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Infix to Postfix/infix-to-postfix.cpp
@@ -0,0 +1,47 @@
+class Solution {
+  public:
+  
+  int prec(char c){
+      if( c == '^') return 3;
+      else if( c == '*' || c == '/') return 2;
+      else if(c == '+' || c == '-' ) return 1;
+      else return -1;
+  }
+  
+  string infixToPostfix(string& s) {
+      stack<char> st;
+      string result;
+      
+      for(int i = 0; i < s.size(); i++) {
+          char c = s[i];
+          if((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9')) {
+              result += c;
+          }
+          else if(c == '(') {
+              st.push(c);
+          }
+          else if(c == ')') {
+              while(!st.empty() && st.top() != '(') {
+                  result += st.top();
+                  st.pop();
+              }
+              if(!st.empty()) st.pop(); // Remove '('
+          }
+          else {
+              while(!st.empty() && prec(c) <= prec(st.top())) {
+                  result += st.top();
+                  st.pop();
+              }
+              st.push(c);
+          }
+      }
+      
+      while(!st.empty()) {
+          result += st.top();
+          st.pop();
+      }
+      
+      return result;
+  }
+};
+
