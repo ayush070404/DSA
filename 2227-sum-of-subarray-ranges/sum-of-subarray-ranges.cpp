2b2acf70bad18205e88c6343196d65e9463f170a
ayush070404 <ayushkbgs4@gmail.com>
2025-05-27 13:51:40 +0530

    Time: 0 ms (100.00%) | Memory: 16 MB (25.60%) - LeetSync

--- 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.cpp
+++ 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.cpp
@@ -1,18 +1,90 @@
 class Solution {
 public:
-    long long subArrayRanges(vector<int>& nums) {
-        int n = nums.size();
-        long long sum = 0;
-
-        for(int i = 0 ; i< n ; i++){
-            int mini = nums[i];
-            int maxi= nums[i] ;
-            for(int j = i+1 ; j< n ; j++){
-                mini = min(mini , nums[j]);
-                maxi = max(maxi , nums[j]);
-                sum += (maxi -mini);
-            }
+    // Previous Less Element
+    vector<int> computePLE(const vector<int>& arr) {
+        int n = arr.size();
+        vector<int> ple(n);
+        stack<int> st;
+        for (int i = 0; i < n; ++i) {
+            while (!st.empty() && arr[st.top()] > arr[i])
+                st.pop();
+            ple[i] = st.empty() ? -1 : st.top();
+            st.push(i);
         }
-    return sum;
+        return ple;
+    }
+
+    // Next Less Element
+    vector<int> computeNLE(const vector<int>& arr) {
+        int n = arr.size();
+        vector<int> nle(n);
+        stack<int> st;
+        for (int i = n - 1; i >= 0; --i) {
+            while (!st.empty() && arr[st.top()] >= arr[i])
+                st.pop();
+            nle[i] = st.empty() ? n : st.top();
+            st.push(i);
+        }
+        return nle;
+    }
+
+    // Previous Greater Element
+    vector<int> computePGE(const vector<int>& arr) {
+        int n = arr.size();
+        vector<int> pge(n);
+        stack<int> st;
+        for (int i = 0; i < n; ++i) {
+            while (!st.empty() && arr[st.top()] < arr[i])
+                st.pop();
+            pge[i] = st.empty() ? -1 : st.top();
+            st.push(i);
+        }
+        return pge;
+    }
+
+    // Next Greater Element
+    vector<int> computeNGE(const vector<int>& arr) {
+        int n = arr.size();
+        vector<int> nge(n);
+        stack<int> st;
+        for (int i = n - 1; i >= 0; --i) {
+            while (!st.empty() && arr[st.top()] <= arr[i])
+                st.pop();
+            nge[i] = st.empty() ? n : st.top();
+            st.push(i);
+        }
+        return nge;
+    }
+
+    long long sumSubarrayMins(vector<int>& arr) {
+        int n = arr.size();
+        vector<int> ple = computePLE(arr);
+        vector<int> nle = computeNLE(arr);
+
+        long long resultMin = 0;
+        for (int i = 0; i < n; ++i) {
+            long long left = i - ple[i];
+            long long right = nle[i] - i;
+            resultMin += (long long)arr[i] * left * right;
+        }
+        return resultMin;
+    }
+
+    long long sumSubarrayMaxs(vector<int>& arr) {
+        int n = arr.size();
+        vector<int> pge = computePGE(arr);
+        vector<int> nge = computeNGE(arr);
+
+        long long resultMax = 0;
+        for (int i = 0; i < n; ++i) {
+            long long left = i - pge[i];
+            long long right = nge[i] - i;
+            resultMax += (long long)arr[i] * left * right;
+        }
+        return resultMax;
+    }
+
+    long long subArrayRanges(vector<int>& nums) {
+        return sumSubarrayMaxs(nums) - sumSubarrayMins(nums);
     }
 };
