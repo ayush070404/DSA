57a6165378528e95d33afd1c5100ec1c94b51902
ayush070404 <ayushkbgs4@gmail.com>
2025-06-01 23:49:18 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (90.88%) - LeetSync

--- 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.cpp
+++ 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.cpp
@@ -1,8 +1,9 @@
 class Solution {
 public:
     int minBitFlips(int start, int goal) {
-        int num = start^goal;
+        int num = start^goal; // helps to identify where the flips are done(XOR)
         int cnt = 0;
+        if(num == 0) return 0;
         while(num != 0){
             num = (num & num -1); // counting setbits
             cnt++;
