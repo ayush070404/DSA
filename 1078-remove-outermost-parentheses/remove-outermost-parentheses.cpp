1444085e4d7a576bf3aa67683bfddfe6051afcef
ayush070404 <ayushkbgs4@gmail.com>
2025-06-02 01:33:24 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (53.79%) - LeetSync

---
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        string ans = "";
+        stack<char> st;
+        for(int i=0;i<s.length();i++){
+            if(s[i] == '('){
+                st.push(s[i]);
+            }
+            if(st.size() > 1){
+                ans.push_back(s[i]);
+            }
+            if(s[i] == ')'){
+                st.pop();
+            }
+        }
+        return ans;
+    }
+};
