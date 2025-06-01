fcebee2f835efa89ec8e727432a13723d4fe0c66
ayush070404 <ayushkbgs4@gmail.com>
2025-06-02 00:23:55 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/K-th Bit is Set or Not/kth-bit-is-set-or-not.cpp
+++ Difficulty: Easy/K-th Bit is Set or Not/kth-bit-is-set-or-not.cpp
@@ -2,10 +2,10 @@ class Solution {
   public:
     bool checkKthBit(int n, int k) {
         // Your code here
-        // if( (n &( 1 << k)) != 0) return true;  // calculating precedence
-        // else return false;
-        
-        if(((n >> k)& 1 ) != 0) return true;
+        if( (n &( 1 << k)) != 0) return true;  // calculating precedence
         else return false;
+        
+        // if(((n >> k)& 1 ) != 0) return true;
+        // else return false;
     }
 };
