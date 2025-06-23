b81ca80ca0208d69769f223f1e79ab137a4b9852
ayush070404 <ayushkbgs4@gmail.com>
2025-06-23 15:50:16 +0530

    Time: 35 ms (98.62%) | Memory: 29.1 MB (52.23%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -5,38 +5,36 @@ public:
         sort(nums.begin(), nums.end());
         vector<vector<int>> ans;
 
-        for(int i = 0; i < n; i++) {
-            // Skip duplicate nums[i]
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
+        for(int i = 0 ; i< n ; i++){
+            if(i> 0 && nums[i] == nums[i-1]) continue;
 
             int left = nums[i];
-            int l = i + 1, r = n - 1;
+            int l = i+1 , r = n-1;
 
-            while (l < r) {
-                int mid = nums[l];
+            while(l< r){
+                int mid= nums[l];
                 int right = nums[r];
 
-                int sum = left + mid + right;
+                int sum = left + mid+ right;
 
-                if (sum == 0) {
-                    ans.push_back({left, mid, right});  /// how to push triplets;
+                if(sum == 0){
+                    ans.push_back({left , mid, right});
 
-                    // Skip duplicates for nums[l] and nums[r]
-                    while (l < r && nums[l] == nums[l + 1]) l++;
-                    while (l < r && nums[r] == nums[r - 1]) r--;
+                    while(l < r && nums[l] == nums[l+1]) l++;
+                    while(l< r && nums[r] == nums[r-1]) r--;
 
                     r--;
                 }
-                else if (sum < 0) {
-                    l++;  // Move left forward to increase sum
+                else if(sum < 0){
+                    l++;
                 }
-                else {
-                    r--;  // Move right backward to decrease sum
+                else{
+                    r--;
                 }
+
             }
             l++;
         }
-
         return ans;
     }
 };
