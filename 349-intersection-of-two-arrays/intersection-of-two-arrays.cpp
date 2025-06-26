09fb8d8c2a4e9532dc8a9959cb35b8bb3a11b2b7
ayush070404 <ayushkbgs4@gmail.com>
2025-06-26 23:52:23 +0530

    Time: 0 ms (100.00%) | Memory: 14.4 MB (38.68%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
-    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) 
-    {
-        set<int> s;
-        vector<int>unionarr;
-        set<int>result;
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        int hash[1001] = {0};  // assuming values in nums1 and nums2 are in range [0, 1000]
+        vector<int> ans;
 
-        for(int n : nums1)
-        {
-            s.insert(n);
+        for (int num : nums1) {
+            hash[num] = 1;  // mark presence in nums1
         }
 
         for (int num : nums2) {
-            if (s.count(num))
-            {
-                result.insert(num); 
+            if (hash[num] == 1) {
+                ans.push_back(num);  // it's common
+                hash[num] = -1;      // mark as used to avoid duplicates
             }
         }
 
-         return vector<int>(result.begin(), result.end());
-
+        return ans;
     }
 };
