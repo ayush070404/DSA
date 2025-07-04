8001eb3016927229278a6c017e34a56595cdde15
ayush070404 <ayushkbgs4@gmail.com>
2025-07-04 17:03:16 +0530

    Time: 12 ms (26.40%) | Memory: 128.3 MB (57.88%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
@@ -1,23 +1,28 @@
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& A) {
-        int n= A.size();
-        vector<int> pos;
-        vector<int> neg;
-        
-        // Segregate the array into positives and negatives.
-        for(int i=0;i<n;i++){
-            
-            if(A[i]>0) pos.push_back(A[i]);
-            else neg.push_back(A[i]);
-        }
-        
-        // Positives on even indices, negatives on odd.
-        for(int i=0;i<n/2;i++){
-            
-            A[2*i] = pos[i];
-            A[2*i+1] = neg[i];
-        }
-    return A;
+        int n = A.size();
+  
+  // Define array for storing the ans separately.
+  vector<int> ans(n,0);
+  
+  // positive elements start from 0 and negative from 1.
+  int posIndex = 0, negIndex = 1;
+  for(int i = 0;i<n;i++){
+      
+      // Fill negative elements in odd indices and inc by 2.
+      if(A[i]<0){
+          ans[negIndex] = A[i];
+          negIndex+=2;
+      }
+      
+      // Fill positive elements in even indices and inc by 2.
+      else{
+          ans[posIndex] = A[i];
+          posIndex+=2;
+      }
+  }
+  
+  return ans;
     }
 };
