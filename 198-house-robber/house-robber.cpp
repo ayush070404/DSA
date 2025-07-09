8a2a4b8ecf4409cdcca3bda47af3c62cfb981077
ayush070404 <ayushkbgs4@gmail.com>
2025-07-09 11:06:49 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (46.26%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,21 +1,32 @@
 class Solution {
 public:
-    int solve (int ind , vector<int> &nums , vector <int> &dp){
-        if(ind == 0 ) return nums[ind];
-        if(ind < 0 ) return 0;
-        if(dp[ind] != -1) return dp[ind];
+    // int solve (int ind , vector<int> &nums , vector <int> &dp){
+    //     if(ind == 0 ) return nums[ind];
+    //     if(ind < 0 ) return 0;
+    //     if(dp[ind] != -1) return dp[ind];
 
-        int take = nums[ind] + solve(ind-2 , nums, dp);
-        int not_pick = solve(ind -1 , nums ,dp);
+    //     int take = nums[ind] + solve(ind-2 , nums, dp);
+    //     int not_pick = solve(ind -1 , nums ,dp);
 
-        dp[ind] = max(take , not_pick);
+    //     dp[ind] = max(take , not_pick);
 
-        return dp[ind];
-    }
+    //     return dp[ind];
+    // }
 
     int rob(vector<int>& nums) {
         int n = nums.size();
         vector <int> dp(n+1 , -1);
-        return solve(n-1 , nums, dp);
+        // return solve(n-1 , nums, dp);
+
+        dp[0] = nums[0];
+
+        for(int ind =1 ; ind<n ; ind++){
+
+            int take = nums[ind] ;
+            if(ind>1) take += dp[ind-2];
+            int not_pick = 0+ dp[ind-1];
+            dp[ind] = max(take , not_pick);
+        }
+    return dp[n-1];
     }
 };
