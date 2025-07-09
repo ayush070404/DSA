ed93af05fb15d466787d5305292d40d0cab7a69a
ayush070404 <ayushkbgs4@gmail.com>
2025-07-09 18:42:41 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (33.65%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -10,12 +10,17 @@ public:
             for(int j =0 ; j< n; j++){
                 int up = 0;
                 int down = 0;
-                if(i == 0 && j == 0 ) continue;
-                if( i>0){
-                    up = dp[i-1][j];
+                if(i == 0 && j == 0 ){
+                     dp[0][0] = 1;
+                     continue;
                 }
-                if(j>0){
-                    down = dp[i][j-1];
+                else{
+                    if( i>0){
+                        up = dp[i-1][j];
+                    }
+                    if(j>0){
+                        down = dp[i][j-1];
+                    }
                 }
                 dp[i][j] = up + down;
             }
