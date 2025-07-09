114429307ae88a2015bfa8bdbb9fa8cac2ff9d17
ayush070404 <ayushkbgs4@gmail.com>
2025-07-09 20:12:34 +0530

    Time: 0 ms (100.00%) | Memory: 16.4 MB (40.20%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -19,7 +19,22 @@ public:
         int m = grid.size();
         int n = grid[0].size();
         vector<vector<int>> dp(m , vector<int>(n , -1));
+        dp[0][0] = grid[0][0];
 
-        return solve(m-1 , n-1 , grid, dp);
+        for(int row = 0 ; row < m ; row++){
+
+            for(int col = 0 ; col < n ; col++){
+                int up =1e9 , left =1e9;
+                if(row == 0 && col == 0) continue;
+                
+                else{
+                     if(row>=1) up = grid[row][col] + dp[row - 1][col];
+                     if(col >=1) left = grid[row][col] + dp[row][col-1];
+                }
+                dp[row][col] = min(up, left);
+            }
+        }
+        return dp[m-1][n-1];
+        // return solve(m-1 , n-1 , grid, dp);
     }
 };
