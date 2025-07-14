2963f1ffaa404d6403f5ea38f2947279eae42fa2
ayush070404 <ayushkbgs4@gmail.com>
2025-07-14 11:42:32 +0530

    Time: 18 ms (59.31%) | Memory: 28.3 MB (61.63%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,27 +1,32 @@
 class Solution {
 public:
 
-    int solve(int ind, vector<int> &arr, int val, vector<vector<int>> &dp) {
-         if (ind == 0) {
-            if (val % arr[0] == 0) return 1;  // Only one way using arr[0] multiple times
-            return 0;
+
+    int solve(int ind , vector<int> &coins , int amt , vector<vector<int >> &dp){
+        if(ind == 0) {
+            if(amt == 0 ) return 1;
+            if( amt % coins[0] == 0) return 1;
+            return 0;  // to prevent oveflow by INT_MAX;
         }
 
-        if (dp[ind][val] != -1) return dp[ind][val];
+        if(dp[ind][amt] != -1) return dp[ind][amt];
 
-        int not_take = solve(ind - 1, arr, val, dp);
+        int not_take =  solve(ind -1 , coins , amt , dp);
         int take = 0;
-        if (arr[ind] <= val)
-            take = solve(ind, arr, val - arr[ind], dp); // stay on same index
+        if(coins[ind] <= amt ) take = solve(ind , coins , amt - coins[ind], dp);
 
-    return dp[ind][val] = not_take + take;
-}
+        dp[ind][amt] = take + not_take;
+
+        return dp[ind][amt];
+    }
 
     int change(int amount, vector<int>& coins) {
-        int n = coins.size();
+         int n = coins.size();
 
-        vector<vector<int>> dp(n , vector<int> (amount+1, -1));
+        vector<vector<int>> dp(n , vector<int> (amount+1 , -1));
 
-        return solve(n-1, coins, amount, dp);
+        int ans = solve( n-1 , coins , amount , dp);
+        // if(ans >=1e9 ) return 0;   // if we can't find the denomination
+        return ans;
     }
 };
