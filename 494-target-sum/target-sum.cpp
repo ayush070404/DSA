b58e7c61523629a3161a50da013543170a71ad18
ayush070404 <ayushkbgs4@gmail.com>
2025-07-14 11:20:33 +0530

    Time: 390 ms (26.45%) | Memory: 89.3 MB (5.91%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,43 +1,24 @@
 class Solution {
 public:
+    int solve(int ind, int target, vector<int>& nums, unordered_map<string, int>& dp) {
+        string key = to_string(ind) + "_" + to_string(target);
+        if (dp.count(key)) return dp[key];
 
-int countPartitions(int n, int d, vector<int> &arr) {
-    // Write your code here.
-    int totSum = accumulate(arr.begin(), arr.end(), 0);
-    
-    if(totSum-d<0) return 0;
-    if((totSum-d)%2!=0) return 0;
-    
-    int target = (totSum-d)/2; // tryingh to devide in two equAL subsets so need even target
-    vector<vector<int>> dp(n, vector<int>(target +1, 0));
-
-    // Base case for the 0th index
-    if (arr[0] == 0) {
-        dp[0][0] = 2; // pick and not pick
-    } else {
-        dp[0][0] = 1; // not pick
-        if (arr[0] <= target)
-            dp[0][arr[0]] = 1; // pick
-    }
-
-    for(int i = 1 ; i <n ; i++) {
-        for( int tar = 0 ; tar <=target ; tar++){
-
-            int not_pick = dp[i-1][tar];
-
-            int pick = 0;
-            if(arr[i]<= tar) pick = dp[i-1][tar- arr[i]];
-
-            dp[i][tar] = (pick+ not_pick);
+        if (ind == 0) {
+            int cnt = 0;
+            if (nums[0] == target) cnt++;
+            if (-nums[0] == target) cnt++;
+            return dp[key] = cnt;
         }
+
+        int take = solve(ind - 1, target - nums[ind], nums, dp);
+        int not_take = solve(ind - 1, target + nums[ind], nums, dp);
+
+        return dp[key] = take + not_take;
     }
-    return dp[n-1][target];
-
-}
-
 
     int findTargetSumWays(vector<int>& nums, int target) {
-        int n = nums.size();
-        return countPartitions(n , target , nums);
+        unordered_map<string, int> dp;
+        return solve(nums.size() - 1, target, nums, dp);
     }
 };
