0cd3ea9c21ad90f777375145fa060d7bcd0b20b3
ayush070404 <ayushkbgs4@gmail.com>
2025-07-20 10:25:30 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (87.29%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -1,34 +1,29 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        ListNode* dummy = new ListNode(-1);
+        ListNode* dummy = new ListNode(0);
         ListNode* tail = dummy;
 
-        while (list1 != nullptr && list2 != nullptr) {
-            if (list1->val <= list2->val) {
-                tail->next = new ListNode(list1->val);  // create new node
+        while (list1 && list2) {
+            if (list1->val < list2->val) {
+                tail->next = list1;
                 list1 = list1->next;
             } else {
-                tail->next = new ListNode(list2->val);  // create new node
+                tail->next = list2;
                 list2 = list2->next;
             }
             tail = tail->next;
         }
 
-        // Copy remaining nodes from list1 or list2
-        while (list1 != nullptr) {
-            tail->next = new ListNode(list1->val);
-            list1 = list1->next;
-            tail = tail->next;
-        }
-        while (list2 != nullptr) {
-            tail->next = new ListNode(list2->val);
-            list2 = list2->next;
-            tail = tail->next;
+        // Use if-else instead of ternary
+        if (list1 != nullptr) {
+            tail->next = list1;
+        } else {
+            tail->next = list2;
         }
 
         ListNode* head = dummy->next;
-        delete dummy;  // clean up dummy node
+        delete dummy;
         return head;
     }
 };
