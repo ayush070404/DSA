9b0629c8c2be1da87dba245ead773654ed4beaf0
ayush070404 <ayushkbgs4@gmail.com>
2025-07-30 18:58:43 +0530

    Time: 6 ms (48.47%) | Memory: 23.8 MB (80.12%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -1,25 +1,18 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-
-        sort(intervals.begin(), intervals.end(), [](const vector<int>& a, const vector<int>& b) {
-            return a[0] < b[0];
-        });
+        int n = intervals.size();
+        sort(intervals.begin(), intervals.end());
 
         vector<vector<int>> merged;
-        vector<int> prev = intervals[0];
-
-        for (int i = 1; i < intervals.size(); ++i) {
-            vector<int> interval = intervals[i];
-            if (interval[0] <= prev[1]) {
-                prev[1] = max(prev[1], interval[1]);
-            } else {
-                merged.push_back(prev);
-                prev = interval;
+        for(int i= 0 ; i<n ; i++){
+            if(merged.empty() || merged.back()[1] <intervals[i][0]){
+                merged.push_back(intervals[i]);
+            }
+            else{
+                merged.back()[1] = max(merged.back()[1], intervals[i][1]);
             }
         }
-
-        merged.push_back(prev);
-        return merged;        
+    return merged;
     }
 };
