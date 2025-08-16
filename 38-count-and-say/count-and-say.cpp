df4d7c9cbad735dfd87ad6386a46b575d33684c4
ayush070404 <ayushkbgs4@gmail.com>
2025-08-16 15:15:44 +0530

    Time: 4 ms (53.43%) | Memory: 9.9 MB (68.62%) - LeetSync

--- 38-count-and-say/count-and-say.cpp
+++ 38-count-and-say/count-and-say.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
 
-        string getNextTerm(const string &s) {
-            string result = "";
-            int count = 1;
-            
-            for (int i = 1; i <= s.size(); i++) {
-                if (i < s.size() && s[i] == s[i - 1]) {
-                    count++;
-                } else {
-                    result += to_string(count) + s[i - 1];
-                    count = 1;
-                }
+    string getNextTerm(string &s){
+        string result = "";
+        int count =1;
+        for(int i=1 ; i<=s.size() ; i++){
+            if(i< s.size() && s[i]==s[i-1]){
+                count++;
             }
-            return result;
+            else{
+                result+= to_string(count)+s[i-1];
+                count =1;
+            }
         }
+        return result;
+    }
 
-        string countAndSay(int n) {
-            // Base case
-            if (n == 1) return "1";
-            
-            // Recursive step: get (n-1)th term, then build nth
-            string prev = countAndSay(n - 1);
-            return getNextTerm(prev);
+    string countAndSay(int n) {
+        string term = "1";
+        for(int i=2 ; i<=n ; i++){
+            term = getNextTerm(term);
         }
+        return term;
+    }
 };
